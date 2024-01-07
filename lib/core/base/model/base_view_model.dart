d01e803986cf7a9345f01964fd19288cad618c8d
betlpolat <polatb9706@gmail.com>
2024-01-07 16:58:56 +0300

    added base dio and view model

--- lib/core/base/model/base_view_model.dart
+++ lib/core/base/model/base_view_model.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:movie_app/core/base/model/base_dio.dart';
 import 'package:movie_app/product/init/network/network_manager.dart';
 
