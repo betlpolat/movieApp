8a7636ff8ff176fbb8dfbb10a6587bcd98c1d96e
betlpolat <polatb9706@gmail.com>
2024-01-08 14:09:10 +0300

    added service test

--- test/service/service_test.dart
+++ test/service/service_test.dart
@@ -1,38 +1,54 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:movie_app/feature/home/service/movie_service.dart';
-import 'package:movie_app/product/init/network/network_manager.dart';
+import 'package:movie_app/core/base/model/base_dio.dart';
+import 'package:movie_app/feature/home/model/movies.dart';
+import 'package:movie_app/product/init/config/app_environment.dart';
+import 'package:movie_app/product/init/config/env_dev.dart';
+import 'package:movie_app/product/init/network/custom_dio.dart';
+import 'package:movie_app/product/utility/constants/path_constant.dart';
 import 'package:movie_app/product/utility/enum/language_code.dart';
 import 'package:movie_app/product/utility/enum/movie_paths.dart';
 import 'package:movie_app/product/utility/extension/movie_paths_extension.dart';
 
 void main() {
-  late IMovieService movieService;
+  late BaseDio customDio;
   setUp(() {
-    movieService = MovieService(NetworkManager.instance.service);
+    AppEnvironment.setup(config: EnvDev());
+    customDio = CustomDio(
+        baseOptions: BaseOptions(
+      baseUrl: PathConstant.basePath,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    ));
   });
   test('Service Test - popular movies', () async {
-    final responsePopular = await movieService
-        .fetchMovieList(MoviePaths.popular.moviePath(LanguageCode.english));
-    expect(responsePopular, isNotNull);
-    expect(responsePopular, isList);
+    final responsePopular = await customDio.dioGet(
+        MoviePaths.popular.moviePath(LanguageCode.english), Movies());
+    final List<Movie>? popularList = responsePopular.results;
+    expect(popularList, isNotNull);
+    expect(popularList, isList);
   });
   test('Service Test - top rated movies', () async {
-    final responseTopRated = await movieService
-        .fetchMovieList(MoviePaths.topRated.moviePath(LanguageCode.english));
-    expect(responseTopRated, isNotNull);
-    expect(responseTopRated, isList);
+    final responseTopRated = await customDio.dioGet(
+        MoviePaths.topRated.moviePath(LanguageCode.english), Movies());
+    final List<Movie>? topRatedList = responseTopRated.results;
+    expect(topRatedList, isNotNull);
+    expect(topRatedList, isList);
   });
   test('Service Test - now playing movies', () async {
-    final responseNowPLaying = await movieService
-        .fetchMovieList(MoviePaths.nowPlaying.moviePath(LanguageCode.english));
-    expect(responseNowPLaying, isNotNull);
-    expect(responseNowPLaying, isList);
+    final responseNowPLaying = await customDio.dioGet(
+        MoviePaths.nowPlaying.moviePath(LanguageCode.english), Movies());
+    final List<Movie>? nowPlayingList = responseNowPLaying.results;
+
+    expect(nowPlayingList, isNotNull);
+    expect(nowPlayingList, isList);
   });
 
-  test('Service Test - search with query is "breakfast" ', () async {
-    final responseSearch = await movieService
-        .fetchMovieList("${MoviePaths.search.searchPath()}&query=breakfast");
-    expect(responseSearch, isNotNull);
-    expect(responseSearch, isList);
-  });
+  // test('Service Test - search with query is "breakfast" ', () async {
+  //   final responseSearch = await customDio.dioGet(
+  //       "${MoviePaths.search.searchPath()}&query=breakfast", Movies());
+  //   expect(responseSearch, isNotNull);
+  //   expect(responseSearch, isList);
+  // });
 }
